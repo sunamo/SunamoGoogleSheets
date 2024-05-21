58f27b964a7e5c1ef06f48e36417229471ec30fe
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-21 07:33:55 +0200

    Before modify modifiers

--- _sunamo/CAG.cs
+++ _sunamo/CAG.cs
@@ -7,23 +7,58 @@ using System.Threading.Tasks;
 namespace SunamoGoogleSheets;
 internal class CAG
 {
-    internal static object LowestCount<T>(List<List<T>> exists)
+    public static int LowestCount<T>(List<List<T>> exists)
     {
-        throw new NotImplementedException();
+        var min = int.MaxValue;
+
+        foreach (var item in exists)
+        {
+            if (min > item.Count)
+            {
+                min = item.Count;
+            }
+        }
+
+        return min;
     }
 
-    internal static int MaxElementsItemsInnerList<T>(List<List<T>> exists)
+    public static int MinElementsItemsInnerList<T>(List<List<T>> exists)
     {
-        throw new NotImplementedException();
+        int min = int.MaxValue;
+
+        foreach (var item in exists)
+        {
+            if (item.Count < min)
+            {
+                min = item.Count;
+            }
+        }
+
+        return min;
     }
 
-    internal static int MinElementsItemsInnerList<T>(List<List<T>> exists)
+    public static int MaxElementsItemsInnerList<T>(List<List<T>> exists)
     {
-        throw new NotImplementedException();
+        int max = 0;
+
+        foreach (var item in exists)
+        {
+            if (item.Count > max)
+            {
+                max = item.Count;
+            }
+        }
+
+        return max;
     }
 
-    internal static List<List<T>> TrimInnersToCount<T>(List<List<T>> exists, object lowest)
+    public static List<List<T>> TrimInnersToCount<T>(List<List<T>> exists, int lowest)
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < exists.Count; i++)
+        {
+            exists[i] = exists[i].Take(lowest).ToList();
+        }
+
+        return exists;
     }
 }
